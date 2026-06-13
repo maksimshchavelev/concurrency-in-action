48dff29d1353dd54b71faa3e8fc883095f732e40
Maksim <maksimshchavelev@gmail.com>
2026-06-13 18:07:17 +0300

    add comment to arguments-ub

--- chapter-2-thread-control/arguments-ub.cpp
+++ chapter-2-thread-control/arguments-ub.cpp
@@ -1,5 +1,5 @@
-#include <thread>
 #include <iostream>
+#include <thread>
 
 void f(const std::string& s) {
     std::cout << s;
@@ -9,6 +9,9 @@ void oops() {
     char buffer[1024];
     std::sprintf(buffer, "%i", 1024);
 
+    // A classic "use after return" stack bug. A pointer to the buffer is copied into the internal storage of `std::thread`, but
+    // by the time a `std::string` is constructed from the raw string, the pointer may be pointing to data that has already been
+    // removed from the stack after the function returned.
     std::thread t(f, buffer);
     t.detach();
 }
@@ -20,4 +23,3 @@ int main() {
 
     return 0;
 }
-
